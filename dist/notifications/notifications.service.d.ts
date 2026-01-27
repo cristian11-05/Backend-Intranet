376dd2d60c838e93ccf9ad0156797e7bc16c1218
Cristian Zavaleta <cristian.zavaleta.b@tecsup.edu.pe>
2026-01-27 13:42:43 -0500

    Elimina dist del repositorio y actualiza Prisma

--- dist/notifications/notifications.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaService } from '../prisma/prisma.service';
-export declare class NotificationsService {
-    private prisma;
-    constructor(prisma: PrismaService);
-    sendPushNotification(userId: number, title: string, body: string): Promise<void>;
-    notifyStatusChange(userId: number, type: string, status: string): Promise<void>;
-}
